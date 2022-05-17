30da760d47c5efa6f993a090c6f7196406722038
ritikasantara <rits.santara@gmail.com>
2022-05-17 10:47:57 +0530

    adding files

---
+++ main.py
@@ -0,0 +1,63 @@
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+def start_timer():
+    count_down(5 * 60)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_display, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+tomato_image = PhotoImage(file="tomato.png")
+canvas = Canvas(width=280, height=224, bg=YELLOW, highlightthickness=0)
+canvas.create_image(144, 112, image=tomato_image)
+timer_display = canvas.create_text(144, 130, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
+canvas.grid(column=1, row=1)
+
+label_timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+label_timer.grid(column=1, row=0)
+
+label_tick = Label(text="✔", fg=GREEN, bg=YELLOW)
+label_tick.grid(column=1, row=3)
+
+button_start = Button(text="Start", font=(FONT_NAME, 20, "bold"), highlightthickness=0, command=start_timer)
+button_start.grid(column=0, row=2)
+
+button_reset = Button(text="Reset", font=(FONT_NAME, 20, "bold"), highlightthickness=0)
+button_reset.grid(column=2, row=2)
+
+
+
+
+
+window.mainloop()
+
